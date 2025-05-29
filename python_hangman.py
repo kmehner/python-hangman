cdc2ecc02e3ff8c357e9bb44cded40e43f8f7754
Katelyn Mehner <kmehner@gmail.com>
2025-05-29 12:05:38 -0600

    final solution + bonus challenges

--- python_hangman.py
+++ python_hangman.py
@@ -24,8 +24,26 @@ fail_count = 0
 
 # Game starts 
 while fail_count < 6: 
+  print("\nWord Progress: \n")  # Adds a line break after the word display
   # Show the word progress 
+
   # The underscore or the letter to show the progress of how many has been guessed 
+  for letter in word.lower():
+    if letter in guessed_letters:
+      print(letter, end=" ")
+    else:
+      print("_", end=" ")
+  
+  print("\n")  # Adds a line break after the word display
+
+  # CHECK IF IT IS A WIN 
+  if all(letter in guessed_letters for letter in word.lower()):
+    print("Congratulations! You guessed the word!")
+    break 
+
+  # for letter in word:
+  #   if letter not in guessed_letters:
+  #       break
 
   # User makes their guess
   guess = input("Guess a letter: ")
@@ -38,14 +56,11 @@ while fail_count < 6:
   else:
     guessed_letters.append(guess)
 
-  # Check if the letter is in the word 
-  if guess in word.lower():
-    print("Good guess! Your letter is in the word")
-  else:
-    print("Sorry, that letter is not in your word")
-    fail_count += 1
+    # Check if the letter is in the word 
+    if guess in word.lower():
+      print("Good guess! Your letter is in the word")
+    else:
+      print("Sorry, that letter is not in your word")
+      fail_count += 1
 
-
-  # CHECK IF IT IS A WIN 
-
-  # break 
+  # BONUS: Notify the user they failed 
